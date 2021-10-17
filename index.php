2e6743d4832ff9d30af197d7c6467fa68903c87d
CristianVelazquezNeva <92596906+CristianVelazquezNeva@users.noreply.github.com>
2021-10-16 21:23:57 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Search-Bus</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+	<style>
+		h1{
+			font-family: verdana;
+		}
+
+		h5{
+			font-weight: 400;
+		}
+	</style>
+</head>
+<body>
+	<div class="py-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<img class="img-fluid d-block mx-auto" src="https://cdn.pixabay.com/photo/2013/03/13/17/49/bus-93219_960_720.png" style="max-height: 250px;">
+					<h1 class="display-3">Search Bus</h1>
+					<h5>Por: Cristian Velazquez</h5>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="py-5">
+		<div class="container">
+			<div class="row">
+				<div class="px-lg-5 col-lg-7" style="padding:0px">
+					<div class="short-div">
+						<h2>Problema:</h2>
+						<p class="mb-3">El lugar en el que vivo, se encuentra alejado de las grandes ciudades y cada vez que necesito trasladarme es normal esperar de 1 minuto a 3 horas para que el autobús llegue, lo que convierte a esta actividad en un proceso molesto y aburrido.</p>
+					</div>
+				    <div class="short-div">
+				    	<h2>Solución:</h2>
+						<p class="mb-3">Desarrollar un sistema de ubicación y rastreo de autobuses para que las personas de las zonas rurales puedan conocer el tiempo que falta para que el próximo autobús que llegue a su localidad y que se dirige a su destino, y así puedan esperar tranquilamente desde la comodidad de sus casas en caso de que este llegase a tardar.</p>
+				    </div>
+				</div>
+				<div class="col-lg-5 d-flex flex-column justify-content-center">
+					<img class="img-fluid d-block" src="https://cdn.pixabay.com/photo/2016/03/22/04/23/map-1272165_960_720.png">
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
